9396c1e355475d559891e160e8204a248ac05d22
kkata <14849535+kkata@users.noreply.github.com>
2020-12-15 18:24:14 +0900

    Events Event modifiers

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,7 @@
 <script>
-let m = { x: 0, y: 0 };
-
-function handleMousemove(event) {
-  m.x = event.clientX;
-  m.y = event.clientY;
+function handleClick() {
+  alert("clicked");
 }
 </script>
 
-<style>
-div {
-  width: 100%;
-  height: 100%;
-}
-</style>
-
-<div on:mousemove="{(e) => (m = { x: e.clientX, y: e.clientY })}">
-  The mouse position is
-  {m.x}
-  x
-  {m.y}
-</div>
+<button on:click|once="{handleClick}"> Click me </button>
