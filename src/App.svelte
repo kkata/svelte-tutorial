c5b07bde6c5a329ca0d512b4fbd10de7aa496fb5
kkata <14849535+kkata@users.noreply.github.com>
2020-12-15 17:21:00 +0900

    Logic Else-if blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
-let user = { loggedIn: false };
-
-function toggle() {
-  user.loggedIn = !user.loggedIn;
-}
+let x = 7;
 </script>
 
-{#if user.loggedIn}
-  <button on:click="{toggle}"> Log out </button>
-{:else}<button on:click="{toggle}"> Log in </button>{/if}
+{#if x > 10}
+  <p>{x} is greater than 10</p>
+{:else}
+  {#if 5 > x}
+    <p>{x} is less than 5</p>
+  {:else}
+    <p>{x} is between 5 and 10</p>
+  {/if}
+{/if}
