94a9e0ed021877558eb209ffef317af8f2cc93b2
kkata <14849535+kkata@users.noreply.github.com>
2020-12-20 18:52:11 +0900

    Lifecycle onMount

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,41 @@
 <script>
-import Keypad from "./Keypad.svelte";
+import { onMount } from "svelte";
 
-let pin;
-$: view = pin ? pin.replace(/\d(?!$)/g, "•") : "enter your pin";
+let photos = [];
 
-function handleSubmit() {
-  alert(`submitted ${pin}`);
-}
+onMount(async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/photos?_limit=20`
+  );
+  photos = await res.json();
+});
 </script>
 
-<h1 style="color: {pin ? '#333' : '#ccc'}">{view}</h1>
+<style>
+.photos {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 8px;
+}
 
-<Keypad bind:value="{pin}" on:submit="{handleSubmit}" />
+figure,
+img {
+  width: 100%;
+  margin: 0;
+}
+</style>
+
+<h1>Photo album</h1>
+
+<div class="photos">
+  {#each photos as photo}
+    <figure>
+      <img src="{photo.thumbnailUrl}" alt="{photo.title}" />
+      <figcaption>{photo.title}</figcaption>
+    </figure>
+  {:else}
+    <!-- this block renders when photos.length === 0 -->
+    <p>loading...</p>
+  {/each}
+</div>
